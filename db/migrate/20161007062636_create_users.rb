f5fdbc3b20181e54a5ac62b5759cbe4c96e13a95
MVishesh <vishesh.ample@gmail.com>
2016-12-15 12:37:41 +0530

    User Table

--- db/migrate/20161007062636_create_users.rb
+++ db/migrate/20161007062636_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username 
+#      t.string :username 
       t.string :email
-      t.string :encrypted_password 
-      t.string :salt
+      t.string :password 
+ #     t.string :salt
 
       
       t.timestamps
